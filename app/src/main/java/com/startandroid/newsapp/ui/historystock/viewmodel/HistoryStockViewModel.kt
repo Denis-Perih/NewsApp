e38d514d777103ebb65ddaf1fe88b603caf2bd19
denis <dperig43421@gmail.com>
2021-11-03 11:37:37 +0200

    addedTickerToHistoryStock

--- app/src/main/java/com/startandroid/newsapp/ui/historystock/viewmodel/HistoryStockViewModel.kt
+++ app/src/main/java/com/startandroid/newsapp/ui/historystock/viewmodel/HistoryStockViewModel.kt
@@ -1,5 +1,6 @@
 package com.startandroid.newsapp.ui.historystock.viewmodel
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,47 +9,47 @@ import com.startandroid.newsapp.data.model.HistoryStockItem
 import com.startandroid.newsapp.data.repository.NewsRepository
 import com.startandroid.newsapp.utils.Result
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ObsoleteCoroutinesApi
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.ticker
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class HistoryStockViewModel(private val repository: NewsRepository) : ViewModel() {
 
     private val historyLiveData = MutableLiveData<Result<HistoryStockItem>>()
     private val historyLiveDataNet = MutableLiveData<String>()
 
-//    private val calendar = GregorianCalendar(1980, Calendar.DECEMBER, 12)
+    private val calendar = GregorianCalendar(1980, Calendar.DECEMBER, 12)
+
+    private val tickerChannel = ticker(1000L)
 
     init {
         viewModelScope.launch {
             if (repository.isNetConnected()) {
-//                goRequestInterval()
-                fetchHistory("1980-12-12")
+                goRequestTicker()
             } else {
                 historyLiveDataNet.postValue("Not net")
             }
         }
     }
 
-//    @SuppressLint("SimpleDateFormat")
-//    private fun goRequestInterval() {
-//        compositeDisposable.add(
-//            Observable.interval(1000L, TimeUnit.MILLISECONDS)
-//                .timeInterval()
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe {
-//                    val dateFormat = SimpleDateFormat("yyyy-MM-dd")
-//
-//                    if (dateFormat.format(calendar.time) == "2018-03-28" ) {
-//                        calendar.set(1980, Calendar.DECEMBER, 12)
-//                        fetchHistory(dateFormat.format(calendar.time))
-//                        calendar.add(Calendar.DAY_OF_YEAR, 1)
-//                    } else {
-//                        fetchHistory(dateFormat.format(calendar.time))
-//                        calendar.add(Calendar.DAY_OF_YEAR, 1)
-//                    }
-//                }
-//        )
-//    }
+    @ObsoleteCoroutinesApi
+    @SuppressLint("SimpleDateFormat")
+    private suspend fun goRequestTicker() {
+        for (event in tickerChannel) {
+            val dateFormat = SimpleDateFormat("yyyy-MM-dd")
+            if (dateFormat.format(calendar.time) == "2018-03-28") {
+                calendar.set(1980, Calendar.DECEMBER, 12)
+                fetchHistory(dateFormat.format(calendar.time))
+                calendar.add(Calendar.DAY_OF_YEAR, 1)
+            } else {
+                fetchHistory(dateFormat.format(calendar.time))
+                calendar.add(Calendar.DAY_OF_YEAR, 1)
+            }
+        }
+    }
 
     private suspend fun fetchHistory(date: String) {
         viewModelScope.launch(Dispatchers.IO) {
@@ -61,9 +62,10 @@ class HistoryStockViewModel(private val repository: NewsRepository) : ViewModel(
         }
     }
 
-//    override fun onCleared() {
-//        compositeDisposable.dispose()
-//    }
+    override fun onCleared() {
+        super.onCleared()
+        tickerChannel.cancel()
+    }
 
     fun getHistoryStockNet() : LiveData<String> {
         return historyLiveDataNet
